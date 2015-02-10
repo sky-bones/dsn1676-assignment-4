15151386ca5fc497c236189c24b3f4187afd054f
Elizabeth Nichols <elizabeth.nichols.95@gmail.com>
2015-02-09 19:02:25 -0500

    Added in information and js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
-
-
 var blueberries = {
+latin: 'Vaccinium corymbosum'
 growsOnShrubs: true,
 energy: '240 kj',
 carbohydrates: '14.49 g',
@@ -9,17 +8,18 @@ protein: '0.74 g'
 };
 
 var grapes = {
+latin: 'Vitis vinifera'
 growsOnShrub: false,
 energy: '288 kJ',
 carbohydrates: '18.1 g',
 protein: '0.72 g'
-}
+};
 
 var redCurrent = {
+latin: 'Ribes rubrum'
 growsOnShrub: true,
 energy: '234 kJ',
 carbohydrates: '13.8 g',
 protein: '1.4 g'
-
-}
+};
 
